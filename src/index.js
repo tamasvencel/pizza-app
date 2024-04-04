136a4c845c958a6ad2171d6b9463a7afee61ed1f
tamasvencel <tamasvenci46@gmail.com>
2024-04-05 01:40:14 +0300

    number of pizzas displayed correctly in the paragraph

--- src/index.js
+++ src/index.js
@@ -96,8 +96,8 @@ function Menu() {
       {numPizzas > 0 ? (
         <>
           <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
+            Authentic Italian cuisine. {numPizzas} creative dishes to choose
+            from. All from our stone oven, all organic, all delicious.
           </p>
 
           <ul className="pizzas">
